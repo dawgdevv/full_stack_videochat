f3b699c69353cbf9ea9856d4dda76a6a5af5bfd6
dawgdevv <nraj02415@gmail.com>
2025-03-02 15:07:37 +0530

    initializing the repo

---
+++ frontend_vst/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { ModeToggle } from "@/components/mode-toggle";
+
+function App() {
+  return (
+    <>
+      <ModeToggle />
+    </>
+  );
+}
+
+export default App;
